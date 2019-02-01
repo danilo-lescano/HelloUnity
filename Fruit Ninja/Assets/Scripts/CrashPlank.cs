339296ab4b3c50c9dc1a29cb4d3b9e35640db53c
danilocaverna <nq3i4fsx@hotmail.com>
2019-02-01 15:51:47 -0300

    plank dynamic size cut

--- Fruit Ninja/Assets/Scripts/CrashPlank.cs
+++ Fruit Ninja/Assets/Scripts/CrashPlank.cs
@@ -6,13 +6,21 @@ public class CrashPlank : MonoBehaviour{
 
     Camera cam;
     Rigidbody2D rb;
+    Component part1, part2;
     
     void Start(){
         cam = Camera.main;
         rb = GetComponent<Rigidbody2D>();
-        Vector2 mpos = Input.mousePosition;
-        Vector2 direction = rb.position - mpos;
-        rb.AddForceAtPosition(direction.normalized, transform.position, ForceMode2D.Force);
+
+        part1 = gameObject.transform.GetChild(0);
+        part2 = gameObject.transform.GetChild(1);
+
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        float size1 = rb.position.x - mousePos.x; if(size1<0) size1 = -size1;
+        if(mousePos.x > rb.position.x) size1 += 1.5f;
+        part1.transform.localScale = new Vector3(1f, size1, 0.1f) * 0.8f;
+        part2.transform.localScale = new Vector3(1f, 3f - size1, 0.1f) * 0.8f;
+
     }
 
     void Update(){
