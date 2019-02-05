c391e75214b3f9aa2d21ff1434c1a0fca89d6322
danilocaverna <nq3i4fsx@hotmail.com>
2019-02-05 16:42:40 -0300

    maozinha que segue

---
+++ DarkWar/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour{
+    public GameObject gb;
+    void Update(){
+        transform.up = new Vector2(
+            Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x,
+            Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y
+        );
+    }
+}
