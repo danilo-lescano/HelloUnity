3321602cfd41dfd131e1905e191c1aa80adeb50b
danilocaverna <nq3i4fsx@hotmail.com>
2019-02-06 14:28:50 -0300

    bullet

--- DarkWar/Assets/Scripts/CameraFollow.cs
+++ DarkWar/Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,13 @@ public class CameraFollow : MonoBehaviour{
     float speed = 0.125f;
 
     void LateUpdate(){
-        //lerp faz com que o objeto acompanhe o outro com uma velocidade menor
+        //Vector3.Lerp(Vector3 origin, Vector3 final, float framesToReach)
+        //framesToReach entre 0 e 1
         transform.position = Vector3.Lerp(
             transform.position,
             new Vector3(target.position.x, transform.position.y, -10),
             speed
         );
+        //transform.LookAt(target);
     }
 }
