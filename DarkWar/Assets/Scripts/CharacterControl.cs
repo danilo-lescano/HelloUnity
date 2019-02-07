0d029c67193564d63ff538af6ee65cb9bed41ef5
danilocaverna <nq3i4fsx@hotmail.com>
2019-02-07 14:55:30 -0300

    charactercontrol fatorado, run melhorado, jump iniciado

--- DarkWar/Assets/Scripts/CharacterControl.cs
+++ DarkWar/Assets/Scripts/CharacterControl.cs
@@ -4,37 +4,29 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour{
 
-    float walkVelocity = 3f;
-    float runVelocity = 8f;
-    bool firstClick = false;
-    bool doubleClick = false;
-    float doubleClickTime = 1f;
+    float walkSpeed = 3f, runSpeed = 8f;
+    string lastKeyDown = "";
+    bool canRun = false;
+    float doubleClickTime = 1f; //cont 1 second from the first click to the second
+    Rigidbody2D rb;
+
+    //clicked key
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void Update(){
-        float vel = walkVelocity;
 
-        if((Input.GetKeyDown("left") || Input.GetKeyDown(KeyCode.A)) && !firstClick){
-            firstClick = true;
-            doubleClick = false;
-            doubleClickTime = 1f;
-        }
-        else if((Input.GetKeyDown("left") || Input.GetKeyDown(KeyCode.A)) && firstClick)
-            doubleClick = true;
-        if((Input.GetKeyDown("right") || Input.GetKeyDown(KeyCode.D)) && !firstClick){
-            firstClick = true;
-            doubleClick = false;
-            doubleClickTime = 1f;
-        }
-        else if((Input.GetKeyDown("right") || Input.GetKeyDown(KeyCode.D)) && firstClick)
-            doubleClick = true;
-        if(firstClick && doubleClickTime > 0)
-            doubleClickTime -= Time.deltaTime;
-        else
-            firstClick = false;
+        CheckDoubleClick();
 
-        if(doubleClick)
-            vel = runVelocity;
+        UpdatePosition();
+        
+        if(Input.GetKeyDown(KeyCode.Space)) //jump
+            rb.AddForce(new Vector2(0f, 500f));
+    }
 
+    void UpdatePosition(){
+        float vel = canRun ? runSpeed : walkSpeed;
 
         transform.position = new Vector3(
             transform.position.x
@@ -43,5 +35,26 @@ public class CharacterControl : MonoBehaviour{
             transform.position.y,
             transform.position.z
         );
+    }
+
+    void CheckDoubleClick(){
+        if(Input.GetKeyDown("left") || Input.GetKeyDown(KeyCode.A)){
+            if(lastKeyDown == "left" && doubleClickTime > 0f)
+                canRun = true;
+            
+            lastKeyDown = "left";
+            doubleClickTime = 1f;
+        }
+        else if(Input.GetKeyDown("right") || Input.GetKeyDown(KeyCode.D)){
+            if(lastKeyDown == "right" && doubleClickTime > 0f)
+                canRun = true;
+            
+            lastKeyDown = "right";
+            doubleClickTime = 1f;
+        }
+        else if(doubleClickTime > 0f)
+            doubleClickTime -= Time.deltaTime;
+        else if(Input.GetAxis("Horizontal") == 0)
+            canRun = false;
     }
 }
