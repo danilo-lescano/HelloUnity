6eab2763a80c435e6c133a8574098631a803404e
danilocaverna <nq3i4fsx@hotmail.com>
2019-04-25 14:35:40 -0400

    upload project

---
+++ TaCerto/Assets/Scripts/lineRenderer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineRenderer : MonoBehaviour{
+    void Update(){
+        if(Input.GetMouseButton(0)){
+            Vector3 pos =  Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+            pos.z = 0f;
+            transform.position = pos;
+        }
+    }
+}
