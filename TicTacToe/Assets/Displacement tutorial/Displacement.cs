660593985020d310c8423acd856e2833be6f24a2
danilocaverna <nq3i4fsx@hotmail.com>
2019-05-07 21:16:54 -0400

    TicTacToe: displacement tutorial

---
+++ TicTacToe/Assets/Displacement tutorial/Displacement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Displacement : MonoBehaviour{
+    float displacementAmount;
+    MeshRenderer mesh;
+    void Start(){
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    void Update(){
+        displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime);
+        mesh.material.SetFloat("_Amount", displacementAmount);
+        if(Input.GetButtonDown("Jump"))
+            displacementAmount += 1f;
+    }
+}
