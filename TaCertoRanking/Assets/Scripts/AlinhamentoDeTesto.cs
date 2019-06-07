dbb0a2228d33ec3bd9816493dc95cfd7c617f959
danilocaverna <nq3i4fsx@hotmail.com>
2019-06-07 10:23:36 -0400

    tmp pro tacertoranking

---
+++ TaCertoRanking/Assets/Scripts/AlinhamentoDeTesto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AlinhamentoDeTesto : MonoBehaviour{
+    public TextMeshProUGUI text;
+    void Start(){
+        text = GetComponent<TextMeshProUGUI>();
+    }
+    void Update(){
+        print(text.text.Length);
+    }
+}
