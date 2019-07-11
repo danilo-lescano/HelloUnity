c954bc932b95893f4a1df46615e5bde17d981baf
danilocaverna <nq3i4fsx@hotmail.com>
2019-07-11 17:04:31 -0400

    Tutorial: 2. Variables and Functions

---
+++ Beginner Scripting/Assets/Script/Script2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour{
+    int myInt = 4;
+    void Start(){
+        int a, b;
+        print(myInt);
+        (a, b) = MultiplyTwoNumbersByTwo(110, 111);
+        print(a + " " + b);
+    }
+
+    (int, int) MultiplyTwoNumbersByTwo(int a, int b){
+        return (a * 2, b * 2);
+    }
+}
