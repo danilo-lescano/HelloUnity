21746a7b0480dc742413722318fe7b401d4e1455
danilocaverna <nq3i4fsx@hotmail.com>
2019-07-12 09:31:44 -0400

    Tutorial: 11. Activating GameObjects

---
+++ Beginner Scripting/Assets/Script/Script10.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script10 : MonoBehaviour{
+    Light myLight;
+    float timeControl = 0f;
+    
+    void Start(){
+        myLight = GetComponent<Light>();
+    }
+
+    void Update(){
+        timeControl += Time.deltaTime;
+        if(timeControl > 1f){
+            myLight.enabled = !myLight.enabled;
+            timeControl -= 1f;
+        }
+    }
+}
---
+++ Beginner Scripting/Assets/Script/Script11.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script11 : MonoBehaviour{
+    public bool child;
+
+    void Start(){
+        string text = "";
+        if(!child)
+            gameObject.SetActive(false);
+
+        text += "I'm ";
+        text += gameObject.activeSelf ? "Activeted." : "not Activeted.";
+        text += "\n";
+        text += "I'm ";
+        text += gameObject.activeInHierarchy ? "Activeted in Hierarchy." : "not Activeted in Hierarchy.";
+        print(text);
+    }
+}
