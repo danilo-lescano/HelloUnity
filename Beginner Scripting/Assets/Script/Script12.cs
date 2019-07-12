554228e7c6a23d17af7c281e33a82ca1801b2ae8
danilocaverna <nq3i4fsx@hotmail.com>
2019-07-12 10:57:28 -0400

    Tutorial: 12. Translate and Rotate

---
+++ Beginner Scripting/Assets/Script/Script12.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script12 : MonoBehaviour{
+    public float velocity = 10f;
+    void Update(){
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.forward * velocity * Time.deltaTime * -1f);
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.forward, velocity * Time.deltaTime * -100f);
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.forward, velocity * Time.deltaTime * 100f);
+    }
+}
