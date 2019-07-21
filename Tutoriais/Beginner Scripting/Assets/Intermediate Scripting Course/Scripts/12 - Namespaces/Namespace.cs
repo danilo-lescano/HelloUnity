36a8445e563a20dababf85c0619d1afc4c6228be
danilocaverna <nq3i4fsx@hotmail.com>
2019-07-21 11:47:25 -0400

    Tutoriais: 12 - namespaces (they live!)

---
+++ Tutoriais/Beginner Scripting/Assets/Intermediate Scripting Course/Scripts/12 - Namespaces/Namespace.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NewNamespace {
+    public class Namespace : MonoBehaviour{
+        void Start(){
+            print("Estou dentro do NewNamespace");
+        }
+
+    }
+}
---
+++ Tutoriais/Beginner Scripting/Assets/Intermediate Scripting Course/Scripts/12 - Namespaces/NoNamespace.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NewNamespace;
+
+public class NoNamespace : MonoBehaviour{
+    void Start(){
+        print("Estou fora do NewNamespace");
+        print(GetComponent<Namespace>().transform.position);
+    }
+}
