9f4e7d110a78ce3486cddf1821c76df3ce098407
danilocaverna <nq3i4fsx@hotmail.com>
2019-07-23 17:30:15 -0400

    Tutoriais: 15 - Quaternions fim

---
+++ Tutoriais/Beginner Scripting/Assets/Intermediate Scripting Course/Scripts/15 - Quaternions/LookAtCarrinhaBizzaro.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCarrinhaBizzaro : MonoBehaviour{
+    public Transform target;
+    void Update(){
+        Vector3 pos = target.position;
+        pos.y = transform.position.y;
+        pos = pos - transform.position;
+        transform.rotation = Quaternion.LookRotation(pos, Vector3.up);
+        //transform.LookAt(target, Vector3.up);
+    }
+}
